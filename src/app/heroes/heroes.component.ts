14efe3eea1da248ff38b8f665e3cfe963ebba036
carlosmding <carlos.pinto@udea.edu.co>
2023-03-10 10:43:16 -0500

    2. Mostrar una lista

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Hero } from '../app.hero';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,14 @@ import { Hero } from '../app.hero';
 })
 export class HeroesComponent {
   
-  hero : Hero = {
-    id: 1, name: "Windstorm"
-  }
-  ;
+  heroes = HEROES;
+  selectedHero?: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+}
+
+  
 }
 
 
